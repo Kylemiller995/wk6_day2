17590bdb1424048c569021c0bd9892b2ae480862
Kyle Miller <kylemiller995@gmail.com>
2017-09-05 21:57:06 +0100

    tired

---
+++ Bear.java
@@ -0,0 +1,20 @@
+import java.util.Hashmap;
+
+
+public class Bear{
+  private HashMap belly;
+  private HashMap journal;
+
+  public Bear()
+  this.belly = new HashMap<Integer, String>;
+  ###this bit confuses me###
+
+  public void eat(String fish, Integer day){
+    belly.put(fish, day);
+  }
+
+  public void journalUpdate(Integer day){
+    
+  }
+
+}
